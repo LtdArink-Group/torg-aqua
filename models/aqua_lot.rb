ade1787637ce8db800eee46db0575b017d0d2835
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-11-21 14:47:06 +1000

    aqua lot refactoring

--- models/aqua_lot.rb
+++ models/aqua_lot.rb
@@ -6,10 +6,10 @@ class AquaLot < Model
     CONSISTENT = 'C'
   end
 
-  attributes :id, :plan_spec_guid, :exec_spec_guid
+  attributes :id, :plan_spec_guid, :spec_guid
 
   PENDING_SQL = <<-sql
-    select al.plan_spec_guid, al.exec_spec_guid
+    select al.plan_spec_guid, al.spec_guid
       from #{table} al
       where al.state = '#{State::PENDING}'
   sql
@@ -20,9 +20,9 @@ class AquaLot < Model
     end
   end
 
-  def initialize(plan_spec_guid, exec_spec_guid)
+  def initialize(plan_spec_guid, spec_guid)
     @plan_spec_guid = DB.guid(plan_spec_guid)
-    @exec_spec_guid = DB.guid(exec_spec_guid)
+    @spec_guid = DB.guid(spec_guid)
   end
 
   def pending
@@ -35,44 +35,44 @@ class AquaLot < Model
 
   private
 
-  attr_reader :plan_spec_guid, :exec_spec_guid
+  attr_reader :plan_spec_guid, :spec_guid
 
   def table
     self.class.table
   end
 
   def state(state)
-    if exec_spec_guid
-      merge_with_exec_spec_id(state)
+    if spec_guid
+      merge_zzc(state)
     else
-      merge_without_exec_spec_id(state)
+      merge(state)
     end
   end
 
-  MERGE_WITH_EXEC_SPEC_ID_SQL = <<-sql
+  MERGE_ZZC_SQL = <<-sql
     merge into #{table} t using
     (select hextoraw(:guid1) plan_spec_guid,
-            hextoraw(:guid2) exec_spec_guid
+            hextoraw(:guid2) spec_guid
        from dual) s
     on (t.plan_spec_guid = s.plan_spec_guid and
-        t.exec_spec_guid = s.exec_spec_guid)
+        t.spec_guid = s.spec_guid)
     when matched then
       update set state = :state
     when not matched then
-      insert (id, plan_spec_guid, exec_spec_guid, state)
+      insert (id, plan_spec_guid, spec_guid, state)
       values (#{table}_seq.nextval, s.plan_spec_guid,
-              s.exec_spec_guid, :state)
+              s.spec_guid, :state)
   sql
 
-  def merge_with_exec_spec_id(state)
-    DB.exec(MERGE_WITH_EXEC_SPEC_ID_SQL, plan_spec_guid, exec_spec_guid, state)
+  def merge_zzc(state)
+    DB.exec(MERGE_ZZC_SQL, plan_spec_guid, spec_guid, state)
     DB.commit
   end
 
-  MERGE_WITHOUT_EXEC_SPEC_ID_SQL = <<-sql
+  MERGE_SQL = <<-sql
     merge into #{table} t using
     (select hextoraw(:guid) plan_spec_guid from dual) s
-    on (t.plan_spec_guid = s.plan_spec_guid and t.exec_spec_guid is null)
+    on (t.plan_spec_guid = s.plan_spec_guid and t.spec_guid is null)
     when matched then
       update set state = :state
     when not matched then
@@ -80,8 +80,8 @@ class AquaLot < Model
       values (#{table}_seq.nextval, s.plan_spec_guid, :state)
   sql
 
-  def merge_without_exec_spec_id(state)
-    DB.exec(MERGE_WITHOUT_EXEC_SPEC_ID_SQL, plan_spec_guid, state)
+  def merge(state)
+    DB.exec(MERGE_SQL, plan_spec_guid, state)
     DB.commit
   end
 end
